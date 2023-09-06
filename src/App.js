60a293a92cb07b6bfb0b88ac102b92219fa3cd0e
pekoisgood <peko.claire@gmail.com>
2023-09-06 19:50:00 +0800

    Fix: decoding issue in non-string variable causing fetch data error

--- src/App.js
+++ src/App.js
@@ -1,114 +1,119 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import Cover from './component/Cover';
-import Quiz from './component/Quiz';
-import { nanoid } from 'nanoid';
-import { decode } from 'html-entities';
-import { queries } from '@testing-library/react';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Cover from "./component/Cover";
+import Quiz from "./component/Quiz";
+import { nanoid } from "nanoid";
+import { decode } from "html-entities";
 
-
-
-// url : https://opentdb.com/api.php?amount=10&category=9
+const url = "https://opentdb.com/api.php?amount=10";
+const shuffleArray = (arr) => arr.sort(() => Math.random() - 0.5);
 
 function App() {
-  const [quizData, setQuizData] = useState({})
-  const [startQuiz, setStartQuiz] = useState(false)
-  const [showScore, setShowScore] = useState(false)
-  const [score, setScore] = useState(0)
-  const [allCheck, setAllCheck] = useState(true)
+  const [quizData, setQuizData] = useState({});
+  const [startQuiz, setStartQuiz] = useState(false);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+  const [allCheck, setAllCheck] = useState(true);
 
-  const url = "https://opentdb.com/api.php?amount=10"
-  const shuffleArray = (arr) => arr.sort(()=> Math.random() - 0.5)
+  useEffect(() => {
+    async function getData() {
+      const res = await fetch(url);
+      const data = await res.json();
+      const quizList = [];
+      data.results.forEach((question) => {
+        quizList.push({
+          id: nanoid(),
+          question: decode(question.question),
+          correct_answer: decode(question.correct_answer),
+          selected: null,
+          checked: false,
+          answers: shuffleArray([
+            ...question.incorrect_answers.map((ans) => decode(ans)),
+            decode(question.correct_answer),
+          ]),
+        });
+      });
+      setQuizData(quizList);
+    }
+    getData();
+  }, []);
 
+  function start() {
+    setStartQuiz(true);
+  }
 
-  useEffect(()=>{
-    async function getData(){
-      const res = await fetch(url)
-      const data = await res.json()
-      let quizList = []
-      data.results.forEach(question => {
-        quizList.push({id:nanoid(), question: decode(question.question), correct_answer: decode(question.correct_answer), selected: null, checked: false, answers: shuffleArray([...decode(question.incorrect_answers), decode(question.correct_answer)])})
+  function handleClickAnswer(id, answer) {
+    setQuizData((questions) =>
+      questions.map((question) => {
+        return question.id === id
+          ? { ...question, selected: answer, checked: true }
+          : question;
       })
-      setQuizData(quizList)
-    }
-    getData()
-  },[ ])
-
-
-
-  function start(){
-    setStartQuiz(true)
+    );
   }
 
-
-  function handleClickAnswer(id, answer){
-    setQuizData(questions => questions.map(question => {
-      return question.id === id ? {...question, selected: answer, checked: true} : question
-    }))
-  }
-
-  function checkScore(){
-    let checked = null
-    quizData.forEach(questions => {
-        if(questions.selected === null){
-          checked = false
-        }
-    })
-    if(checked === false){
-      setAllCheck(false)
-    }else if(checked === null){
-      setShowScore(prev => !prev)
-      quizData.forEach(questions => {
-          return questions.selected === questions.correct_answer ? setScore(prevScore=> prevScore+1) : score
-    })
+  function checkScore() {
+    let checked = null;
+    quizData.forEach((questions) => {
+      if (questions.selected === null) {
+        checked = false;
+      }
+    });
+    if (checked === false) {
+      setAllCheck(false);
+    } else if (checked === null) {
+      setShowScore((prev) => !prev);
+      quizData.forEach((questions) => {
+        return questions.selected === questions.correct_answer
+          ? setScore((prevScore) => prevScore + 1)
+          : score;
+      });
     }
   }
-  
-  function playAgain(){
-    setQuizData(questions => questions.map(question => {
-      return {...question, selected: null, checked: false}
-    }))
-    setScore(0)
-    setShowScore(prev => !prev)
-    setStartQuiz(prev => !prev)
-    setAllCheck(true)
+
+  function playAgain() {
+    setQuizData((questions) =>
+      questions.map((question) => {
+        return { ...question, selected: null, checked: false };
+      })
+    );
+    setScore(0);
+    setShowScore((prev) => !prev);
+    setStartQuiz((prev) => !prev);
+    setAllCheck(true);
   }
 
-  for(let i =0; i < quizData.length; i++){
-    console.log(quizData[i].correct_answer)
-  }
-  console.log(score)
-  return ( quizData.length > 0 ? (
+  return quizData.length > 0 ? (
     <main>
       <div className="container">
         <div className="bg-yellow-block"></div>
-          {startQuiz ? 
-          <Quiz 
-            quizData={quizData} 
-            showScore={showScore} 
+        {startQuiz ? (
+          <Quiz
+            quizData={quizData}
+            showScore={showScore}
             handleClickAnswer={handleClickAnswer}
             score={score}
             checkScore={checkScore}
             playAgain={playAgain}
             allCheck={allCheck}
-          /> 
-          : <Cover start={start}/>}
+          />
+        ) : (
+          <Cover start={start} />
+        )}
         <div className="bg-blue-block"></div>
       </div>
     </main>
   ) : (
     <main>
-        <div className='container'>
-          <div className="bg-yellow-block"></div>
-          <div className='load'>
-            <h1>Loading...</h1>
-            <p>If the page is loading too slow, please reload the page.</p>
-          </div>
-          <div className="bg-blue-block"></div>
+      <div className="container">
+        <div className="bg-yellow-block"></div>
+        <div className="load">
+          <h1>Loading...</h1>
         </div>
+        <div className="bg-blue-block"></div>
+      </div>
     </main>
-  )  
-  )
+  );
 }
 
 export default App;
--- src/component/Quiz.js
+++ src/component/Quiz.js
@@ -1,53 +1,56 @@
-import React from 'react';
-import { decode } from 'html-entities';
-import { nanoid } from 'nanoid';
+import React from "react";
 
-
-
-export default function Quiz(props){
-    
-
-
-    const quizes = props.quizData.map(item => {
-        
-        return(
-            <div className='quiz-container'>
-                <h2 className='quiz-question'>{item.question}</h2>
-                <div className='quiz-ans-container'>
-                    {item.answers.map(ans => 
-                    <button 
-                        onClick={()=>props.handleClickAnswer(item.id, ans)} 
-                        className={`${ props.showScore ?  
-                        ans === item.correct_answer ? "quiz-ans correct_ans" :
-                            ans === item.selected && ans !== item.correct_answer ? "quiz-ans incorrect_ans" : "quiz-ans other_incorrect_ans"
-                        : 
-                        item.selected === ans ? "quiz-ans selected" : "quiz-ans" }`} 
-                        // className={item.selected === ans ? "quiz-ans selected" : "quiz-ans"} 
-                    >
-                        {ans}
-                    </button>)}
-                </div>
-                <hr />
-            </div>
-        )
-    })
-    console.log(props.allCheck)
-
-    return(
-        <div className='quizes-container'>
-            {quizes}
-            
-            {props.showScore ? 
-            <div className='quiz-score-container'>
-                <h3>You scored {props.score}/10 correct answer</h3>
-                <button className="btn" onClick={props.playAgain}>Play again</button>
-            </div> : 
-            <div className='quiz-check-answer-container'>
-                <button className='btn' onClick={props.checkScore}>Check Answer</button>
-                {/* check if every question is answered */}
-                {props.allCheck === false && <p>*** Please answer every question ***</p>}
-            </div>
-            }
+export default function Quiz(props) {
+  const quizes = props.quizData.map((item, index) => {
+    return (
+      <div className="quiz-container" key={index}>
+        <h2 className="quiz-question">{item.question}</h2>
+        <div className="quiz-ans-container">
+          {item.answers.map((ans, index) => (
+            <button
+              key={index}
+              onClick={() => props.handleClickAnswer(item.id, ans)}
+              className={`${
+                props.showScore
+                  ? ans === item.correct_answer
+                    ? "quiz-ans correct_ans"
+                    : ans === item.selected && ans !== item.correct_answer
+                    ? "quiz-ans incorrect_ans"
+                    : "quiz-ans other_incorrect_ans"
+                  : item.selected === ans
+                  ? "quiz-ans selected"
+                  : "quiz-ans"
+              }`}
+            >
+              {ans}
+            </button>
+          ))}
         </div>
-    )
+        <hr />
+      </div>
+    );
+  });
+  return (
+    <div className="quizes-container">
+      {quizes}
+
+      {props.showScore ? (
+        <div className="quiz-score-container">
+          <h3>You scored {props.score}/10 correct answer</h3>
+          <button className="btn" onClick={props.playAgain}>
+            Play again
+          </button>
+        </div>
+      ) : (
+        <div className="quiz-check-answer-container">
+          <button className="btn" onClick={props.checkScore}>
+            Check Answer
+          </button>
+          {props.allCheck === false && (
+            <p>*** Please answer every question ***</p>
+          )}
+        </div>
+      )}
+    </div>
+  );
 }
